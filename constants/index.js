bf9952b13431079c92ec658d334d0509be3f741b
DeenRamah <deenramahh66@gmail.com>
2024-07-03 15:17:09 +0300

    first commit

---
+++ constants/index.js
@@ -0,0 +1,44 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/assets/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/search.svg",
+    route: "/search",
+    label: "Hunt Box",
+  },
+  {
+    imgURL: "/assets/heart.svg",
+    route: "/activity",
+    label: "Vibe",
+  },
+  {
+    imgURL: "/assets/create.svg",
+    route: "/create-thread",
+    label: "Create Buzz",
+  },
+  {
+    imgURL: "/assets/community.svg",
+    route: "/communities",
+    label: "Cohorts",
+  },
+  {
+    imgURL: "/assets/user.svg",
+    route: "/profile",
+    label: "Profile",
+  },
+];
+
+export const profileTabs = [
+  { value: "threads", label: "Buzz", icon: "/assets/reply.svg" },
+  { value: "replies", label: "Replies", icon: "/assets/members.svg" },
+  { value: "tagged", label: "Tagged", icon: "/assets/tag.svg" },
+];
+
+export const communityTabs = [
+  { value: "threads", label: "Buzz", icon: "/assets/reply.svg" },
+  { value: "members", label: "Retorts", icon: "/assets/members.svg" },
+  { value: "requests", label: "Proposals", icon: "/assets/request.svg" },
+];
